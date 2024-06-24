1faef21d1225a67e1c3ee8aeca70bc99d90d99c3
choboYo <tjddlsmn96@gmail.com>
2024-06-24 14:12:12 +0900

    jsp connect, main.jsp

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -8,8 +8,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class UsrHomeController {
 	
 	@GetMapping("/usr/home/main")
-	@ResponseBody
 	public String showMain() {
-		return "안녕하세요";
+		return "/usr/home/main";
+	}
+	
+	@GetMapping("/")
+	public String showRoot() {
+		// redirect를 통해서 http에서 https로 옮기는 방식을 적용할때 필요한 작업이다.
+		return "redirect:/usr/home/main";
 	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/usr/home/main.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>메인</title>
+</head>
+<body>
+<div>
+	<h1 style= "font-size: 100px">
+		메인 페이지
+	</h1>
+</div>
+
+</body>
+</html>
